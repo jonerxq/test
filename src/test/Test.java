693686183b68183d5e91a7504a0525dbe2d43936
zhiqiang.jiang <zhiqiang.jiang>
2014-11-04 17:11:48 +0800

    删除.classpath;.project 等文件 在gitignore中过滤文件夹.idea  以及impl文件 增加输入输入参数的测试用力

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,7 +2,8 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello world!");  
+		System.out.println("hello world!");
+		System.out.println("args = [" + args + "]");
 	}
 
 }
